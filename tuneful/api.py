4af281e6f6ca818c3c728dd0d021f4d28c560966
cenyia <chidienyia@hotmail.com>
2016-06-03 02:57:48 +0000

    first commit

--- tuneful/api.py
+++ tuneful/api.py
@@ -10,5 +10,41 @@ from . import decorators
 from tuneful import app
 from .database import session
 from .utils import upload_path
+from .models import Song, File
+
+@app.route("/api/songs/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get(id):
+    """ Get a list of songs """
+
+    #Get the songs from the database
+    songs = session.query(Song).order_by(Song.id)
+
+    # Return the post as JSON
+    data = json.dumps(songs.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+    
 
 
+@app.route("/uploads/<filename>", methods=["GET"])
+def uploaded_file(filename):
+    return send_from_directory(upload_path(), filename)
+    
+    
+@app.route("/api/files", methods=["POST"])
+@decorators.require("multipart/form-data")
+@decorators.accept("application/json")
+def file_post():
+    file = request.files.get("file")
+    if not file:
+        data = {"message": "Could not find file data"}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    filename = secure_filename(file.filename)
+    db_file = models.File(filename=filename)
+    session.add(db_file)
+    session.commit()
+    file.save(upload_path(filename))
+
+    data = db_file.as_dictionary()
+    return Response(json.dumps(data), 201, mimetype="application/json")
--- tuneful/models.py
+++ tuneful/models.py
@@ -7,3 +7,36 @@ from sqlalchemy.orm import relationship
 from tuneful import app
 from .database import Base, engine
 
+class Song(Base):
+    __tablename__ = "songs"
+
+    id = Column(Integer, primary_key=True)
+    file = Column(String(128))
+    name = Column(String(1024))
+    
+    def as_dictionary(self):
+        song = {
+            "id": self.id,
+            "file": {"id": self.id,
+            "name": self.body
+            }
+        }
+        return song
+        
+        
+class File(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True)
+    filename = Column(String(1024))        
+    
+    def as_dictionary(self):
+        #file = {
+            #"id": self.id,
+            #"name": self.body
+        #}
+        return {
+            "id": self.id,
+            "name": self.filename,
+            "path": url_for("uploaded_file", filename=self.filename)
+        }
